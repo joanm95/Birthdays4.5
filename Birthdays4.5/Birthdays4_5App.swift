8df12d6112befa30241e14d321874cdec2d2827b
Joan <joanm95@gmail.com>
2025-08-08 09:43:44 -0400

    Initial Commit

---
+++ Birthdays4.5/Birthdays4_5App.swift
@@ -0,0 +1,17 @@
+//
+//  Birthdays4_5App.swift
+//  Birthdays4.5
+//
+//  Created by Scholar on 8/8/25.
+//
+
+import SwiftUI
+
+@main
+struct Birthdays4_5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
